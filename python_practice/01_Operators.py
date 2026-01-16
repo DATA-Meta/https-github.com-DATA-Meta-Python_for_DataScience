d9c6aff183d9df41ffb4015e7fd8bbf9075d294c
data_holick <111300107+DATA-Meta@users.noreply.github.com>
2026-01-16 11:23:46 +0100

    Added explanation to 01_Operators.py

--- python_practice/01_Operators.py
+++ python_practice/01_Operators.py
@@ -1,21 +1,28 @@
-# # how to write a string
-# print("we are learning python with codanics")
-# print('ammar')
-# print('''Ammar 
-#       codanics''')
+# 📘 File: 01_Operators.py
+# This script demonstrates basic Python operators:
+# - Arithmetic operators: +, -, *, /, %, **, //
+# - Comparison operators: ==, !=, >, <, >=, <=
+# - Logical operators: and, or, not
+# Each section includes simple examples to show how these operators behave with different values.
 
-# print(2+3) #This is called integer
+# how to write a string
+print("we are learning python with codanics")
+print('ammar')
+print('''Ammar 
+      codanics''')
 
-# print(3.5-2.1) #This is type Float
+print(2+3)  # This is called integer
+
+print(3.5-2.1)  # This is type Float
 
 # Operators list of integers in square bracket
- 
-a = [1,2,3]
-b = [4,5,6]
-c = [4,5,6]
- 
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [4, 5, 6]
+
 print(a)
 print(b)
 print(a+b)
-print(a==b)
-print(b==c) #these are boolean operators as aswers comes in true or false
+print(a == b)
+print(b == c)  # these are boolean operators as aswers comes in true or false
